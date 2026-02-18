ca9dc6909e7d980d8d7f0a22db2bcf47e896a2ea
dikotl <188217911+dikotl@users.noreply.github.com>
2026-02-18 23:11:40 +0200

    Task 1

---
+++ Task1Timer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Stopwatch = System.Diagnostics.Stopwatch;
+
+namespace Task1;
+
+class Timer
+{
+    public Timer(TimeSpan delay, Action callback)
+    {
+        var t = new Thread(() =>
+        {
+            while (true)
+            {
+                callback();
+                Thread.Sleep(delay);
+            }
+        });
+        t.IsBackground = true;
+        t.Start();
+    }
+}
+
+class Program
+{
+    static async Task Main()
+    {
+        int n = 0;
+        _ = new Timer(TimeSpan.FromMilliseconds(100), () => { Console.WriteLine(++n); });
+
+        Thread.Sleep(TimeSpan.FromSeconds(5));
+    }
+}
