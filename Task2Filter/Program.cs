4c17f51edbf7ce5216656937a99d4352a1243db1
dikotl <188217911+dikotl@users.noreply.github.com>
2026-02-18 23:46:16 +0200

    Task 2

---
+++ Task2Filter/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        Console.WriteLine(string.Join(", ", nums.Where(num => num % 2 == 0)));
+        Console.WriteLine(string.Join(", ", Filter(nums, num => num % 2 == 0)));
+    }
+
+    static IEnumerable<T> Filter<T>(IEnumerable<T> values, Func<T, bool> predicate)
+    {
+        foreach (var value in values)
+        {
+            if (predicate(value))
+            {
+                yield return value;
+            }
+        }
+    }
+}
